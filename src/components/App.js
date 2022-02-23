2fd45cd3b22d9020133ab2d817c81af26a911256
MohammadHossein <mammadjahanbakhsh@yahoo.com>
2022-02-23 14:54:02 +0330

    configure for lazy loading

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React , {Suspense} from 'react';
 import { BrowserRouter , Route ,Router ,Switch} from 'react-router-dom'; //BrowserRouter is mostly for Before creating our custom history objects and has no use anymore
-import streamCreate from './todos/NoteCreate';
-import streamEdit from './todos/NoteEdit';
-import StreamDelete from './todos/NoteDelete';
-import streamList from './todos/NoteList';
-import streamShow from './todos/NoteShow';
+import NoteList from './todos/NoteList';
 import Header from './Header';
 import history from '../history';
+import {Spinner} from 'react-bootstrap'
 
+
+
+const NoteCreate = React.lazy(()=>import('./todos/NoteCreate'))
+const NoteShow = React.lazy(()=>import('./todos/NoteShow'))
+const NoteEdit = React.lazy(()=>import('./todos/NoteEdit'))
+const NoteDelete = React.lazy(()=>import('./todos/NoteDelete'))
 
 
 //to be honest we are tricking users by react router.with this kind of navigation we are actually showing and hiding 
@@ -22,11 +25,15 @@ const App = ()=>{
         <Router history={history}>
             <Header/>
             <Switch>
-                <Route path='/' exact component={streamList}/>  {/*becuase in above ;we pass these components,each component has some default props provided by reacr router;like:history,location,match.. */}
-                <Route path='/todo/new'  component={streamCreate}/>
-                <Route path='/todo/edit/:id'  component={streamEdit}/>{/*special charachter is ":" and we could say anything instead of id .whatever we say will be the key of the match params in props for that component*/}
-                <Route path='/todo/delete/:id'  component={StreamDelete}/>
-                <Route path='/todo/:id'  component={streamShow}/>
+                <Suspense fallback={<h1 className='text-center'><Spinner animation="border" size='bg'/></h1>}>
+
+                    <Route path='/' exact component={NoteList}/>  {/*becuase in above ;we pass these components,each component has some default props provided by reacr router;like:history,location,match.. */}
+                    <Route path='/todo/new'  component={NoteCreate}/>
+                    <Route path='/todo/edit/:id'  component={NoteEdit}/>{/*special charachter is ":" and we could say anything instead of id .whatever we say will be the key of the match params in props for that component*/}
+                    <Route path='/todo/delete/:id'  component={NoteDelete}/>
+                    <Route path='/todo/:id'  component={NoteShow}/>
+
+                </Suspense>
             </Switch>
         </Router>
     </div>
